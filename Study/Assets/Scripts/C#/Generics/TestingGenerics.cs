75dcced9f7595814d8a089032d9708fcdbac6e06
gseven-gdv <zerogravitystd@gmail.com>
2021-03-10 19:09:43 -0300

    Update TestingGenerics.cs

--- Study/Assets/Scripts/C#/Generics/TestingGenerics.cs
+++ Study/Assets/Scripts/C#/Generics/TestingGenerics.cs
@@ -6,14 +6,24 @@ public class TestingGenerics : MonoBehaviour
 {
     void Start()
     {
-        int[] intArray = CreateArray(5, 6);
+        int[] intArray = CreateArray<int>(5, 6);
+        string[] stringArray = CreateArray<string>("Hello", "World");
 
-        Debug.Log($"{ intArray.Length }  { intArray[0] }  { intArray[1] }");
+        Debug.Log($"Int Array: Length: { intArray.Length }, Index 0: { intArray[0] }, Index 1: { intArray[1] }");
+        Debug.Log($"Int Array: Length: { stringArray.Length }, Index 0: { stringArray[0] }, Index 1: { stringArray[1] }");
     }
 
-    private int[] CreateArray(int firstElement, int secondElement)
+    /// <summary>
+    /// The Generic Signature makes the type of the fuction something like auto in c++,
+    /// or a normal variable in javascript
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="firstElement"></param>
+    /// <param name="secondElement"></param>
+    /// <returns></returns>
+    private T[] CreateArray<T>(T firstElement, T secondElement)
     {
-        return new int[] {
+        return new T[] {
             firstElement, secondElement
         };
      }
