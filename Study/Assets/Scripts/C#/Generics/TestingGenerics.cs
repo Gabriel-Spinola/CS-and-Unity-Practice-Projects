ada0fe0d4e55850c2d51e5d262dea291f8928cde
gseven-gdv <zerogravitystd@gmail.com>
2021-03-10 19:03:47 -0300

    Working with generics

---
+++ Study/Assets/Scripts/C#/Generics/TestingGenerics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingGenerics : MonoBehaviour
+{
+    void Start()
+    {
+        int[] intArray = CreateArray(5, 6);
+
+        Debug.Log($"{ intArray.Length }  { intArray[0] }  { intArray[1] }");
+    }
+
+    private int[] CreateArray(int firstElement, int secondElement)
+    {
+        return new int[] {
+            firstElement, secondElement
+        };
+     }
+}
