96e8db061375a9764639266aed18f921386b6e99
gseven-gdv <zerogravitystd@gmail.com>
2021-03-11 19:28:00 -0300

    Frog.cs

---
+++ Sample Game/Assets/Scripts/Enemies/Frog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
