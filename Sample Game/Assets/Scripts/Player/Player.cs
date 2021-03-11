0f089772bb77543e048d127dcf2f4c6ce945f2c6
gseven-gdv <zerogravitystd@gmail.com>
2021-03-11 12:03:16 -0300

    Player Movement

---
+++ Sample Game/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float counterMovement;
+
+    float xAxis;
+
+    float threshold = 0.3f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb.velocity.magnitude >= maxSpeed)
+            return;
+
+        rb.AddForce(xAxis * Vector3.right * moveSpeed * Time.fixedDeltaTime);
+
+        if(
+            (Mathf.Abs(rb.velocity.x) >= threshold  && xAxis == 0 ) || 
+            (Mathf.Abs(rb.velocity.x) <= threshold && xAxis == 0)
+        ) {
+            rb.AddForce(rb.velocity.x * rb.position * Time.fixedDeltaTime * counterMovement);
+        }
+    }
+}
