fb983688524a57b4ef2c5831ae3442c737650fae
gseven-gdv <zerogravitystd@gmail.com>
2021-05-17 20:19:54 -0300

    Adding some sort of "culling"

--- Minecraft/Assets/Scripts/Chunk.cs
+++ Minecraft/Assets/Scripts/Chunk.cs
@@ -13,28 +13,91 @@ public class Chunk : MonoBehaviour
         meshFilter = GetComponent<MeshFilter>();
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private List<Vector3> vertices = new List<Vector3>();
+    private List<int> triangles = new List<int>();
+    private List<Vector2> uvs = new List<Vector2>();
+
+    private bool[,,] voxelMap = new bool[VoxelData.chunkWidth, VoxelData.chunkHeight, VoxelData.chunkWidth];
+
+    private int vertexIndex = 0;
+
+    private void Start()
     {
-        int vertexIndex = 0;
+        PopulateVoxelMap();
+        CreateMeshChunkData();
+        CreateMesh();
+    }
 
-        Mesh mesh = new Mesh();
+    private void PopulateVoxelMap() 
+    {
+        for (int y = 0; y < VoxelData.chunkHeight; y++)
+            for (int x = 0; x < VoxelData.chunkWidth; x++)
+                for (int z = 0; z < VoxelData.chunkWidth; z++) {
+                    voxelMap[x, y, z] = true;
+                }
+    }
 
-        List<Vector3> vertices = new List<Vector3>();
-        List<Vector2> uvs = new List<Vector2>();
-        List<int> triangles = new List<int>();
+    private void CreateMeshChunkData() 
+    {
+        for (int y = 0; y < VoxelData.chunkHeight; y++)
+            for (int x = 0; x < VoxelData.chunkWidth; x++)
+                for (int z = 0; z < VoxelData.chunkWidth; z++) {
+                    AddVoxelDataToChunk(new Vector3(x, y, z));
+                }
+    }
 
+    /// <summary>
+    /// Check if we have a voxel in a certain position
+    /// </summary>
+    private bool hasVoxelThere(Vector3 pos) 
+    {
+        int x = Mathf.FloorToInt(pos.x),
+            y = Mathf.FloorToInt(pos.y),
+            z = Mathf.FloorToInt(pos.z);
+
+        // Set the constraints
+        if (
+            (x < 0 || x > VoxelData.chunkWidth  - 1) ||
+            (y < 0 || y > VoxelData.chunkHeight - 1) ||
+            (z < 0 || z > VoxelData.chunkWidth  - 1)
+        ) {
+            return false;
+        }
+
+        return voxelMap[x, y, z];
+    }
+
+    /// <summary>
+    /// * if theres is no voxel againts that face:
+    /// Get the current triangle
+    /// Add the vertices, uvs and triangles to its list
+    /// Set the position of the voxels
+    /// </summary>
+    private void AddVoxelDataToChunk(Vector3 pos) 
+    {
         for (int i = 0; i < VoxelData.voxelTris.GetLength(0); i++) {
-            for (var j = 0; j < VoxelData.voxelTris.GetLength(1); j++) {
-                int triangleIndex = VoxelData.voxelTris[i, j];
+            if (!hasVoxelThere(pos + VoxelData.faceChecks[i])) {
+                for (int j = 0; j < VoxelData.voxelTris.GetLength(1); j++) {
+                    vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[i, j]]);
+                    uvs.Add(VoxelData.voxelUvs[j]);
+                }
 
-                vertices.Add(VoxelData.voxelVerts[triangleIndex]);
-                triangles.Add(vertexIndex);
-                uvs.Add(Vector2.zero);
-                
-                vertexIndex++;
+                for (int j = 0; j < 6; j++) {
+                    triangles.Add(vertexIndex + VoxelData.vertexOrder[j]);
+                }
+
+                vertexIndex += 4;
             }
         }
+    }
+
+
+    /// <summary>
+    /// Create the mesh and recalculate the normals
+    /// </summary>
+    private void CreateMesh() 
+    {
+        Mesh mesh = new Mesh();
 
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
--- Minecraft/Assets/Scripts/VoxelData.cs
+++ Minecraft/Assets/Scripts/VoxelData.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// Store the data that we will need to use when drawing the voxels
+// Store the data that we will need to use when rendering the voxels
 public static class VoxelData
 {
-    // Store the voxel vertices locations
+    public static readonly int chunkWidth  = 16;
+    public static readonly int chunkHeight = 16;
+
+    // Store the voxels vertices locations
     public static readonly Vector3[] voxelVerts = new Vector3[8] {
         new Vector3(0.0f, 0.0f, 0.0f),
         new Vector3(1.0f, 0.0f, 0.0f),
@@ -17,13 +20,36 @@ public static class VoxelData
         new Vector3(0.0f, 1.0f, 1.0f),
     };
 
-    // Store the voxel triangles ("faces") 
-    public static readonly int[,] voxelTris = new int[6, 6] {
-        { 0, 3, 1, 1, 3, 2 }, // Back   Face
-        { 5, 6, 4, 4, 6, 7 }, // Front  Face
-        { 3, 7, 2, 2, 7, 6 }, // Top    Face
-        { 1, 5, 0, 0, 5, 4 }, // Bottom Face
-        { 4, 7, 0, 0, 7, 3 }, // Left   Face
-        { 1, 2, 5, 5, 2, 6 }, // Right  Face
+    // Get the coordinates to check the faces of the voxel
+    public static readonly Vector3[] faceChecks = new Vector3[6] {
+        new Vector3( 0.0f,  0.0f, -1.0f), // Check Back   Face
+        new Vector3( 0.0f,  0.0f,  1.0f), // Check Front  Face
+        new Vector3( 0.0f,  1.0f,  0.0f), // Check Bottom Face
+        new Vector3( 0.0f, -1.0f,  0.0f), // Check Top    Face
+        new Vector3(-1.0f,  0.0f,  0.0f), // Check Left   Face
+        new Vector3( 1.0f,  0.0f,  0.0f), // Check Right  Face
+    };
+
+    // Store the voxels triangles ("faces") 
+    public static readonly int[,] voxelTris = new int[6, 4] {
+        // 0 1 2 2 1 3
+        { 0, 3, 1, 2 }, // Back   Face
+        { 5, 6, 4, 7 }, // Front  Face
+        { 3, 7, 2, 6 }, // Top    Face
+        { 1, 5, 0, 4 }, // Bottom Face
+        { 4, 7, 0, 3 }, // Left   Face
+        { 1, 2, 5, 6 }, // Right  Face
+    };
+
+    // Store the voxels uvs positions
+    public static readonly Vector2[] voxelUvs = new Vector2[4] {
+        new Vector2(0.0f, 0.0f),
+        new Vector2(0.0f, 1.0f),
+        new Vector2(1.0f, 0.0f),
+        new Vector2(1.0f, 1.0f),
+    };
+
+    public static readonly int[] vertexOrder = new int[6] { 
+        0, 1, 2, 2, 1, 3
     };
 }
