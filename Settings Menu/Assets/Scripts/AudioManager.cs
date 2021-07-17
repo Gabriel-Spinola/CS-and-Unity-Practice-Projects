e53bbe7a96210268db9df2f5eca4dbc6ac88eae2
gseven-gdv <zerogravitystd@gmail.com>
2021-07-17 16:51:20 -0300

    Starting Audio Manager

---
+++ Settings Menu/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private float masterVolumePercent = 1f;
+    private float musicVolumePercent = 1f;
+    private float sfxVolumePercent = 1f;
+
+    public void Play(AudioClip clip, Vector3 pos)
+    {
+        AudioSource.PlayClipAtPoint(clip, pos, sfxVolumePercent * masterVolumePercent);
+    }
+}
