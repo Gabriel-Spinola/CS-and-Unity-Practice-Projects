c0ab8aa54c1bfa7c808be9670e2baf6b511cdbee
gseven-gdv <zerogravitystd@gmail.com>
2021-07-20 14:35:28 -0300

    Update Collision.cs

--- Training/Assets/Scripts/Player/Collision.cs
+++ Training/Assets/Scripts/Player/Collision.cs
@@ -4,25 +4,35 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private LayerMask whatIsBlocks;
 
+    [Header("Horizontal Collision")]
     [SerializeField] private Vector2 rightColOffset;
     [SerializeField] private Vector2 leftColOffset;
 
+    [SerializeField] private float horizontalColRadius = .5f;
+    [SerializeField] private float bottomColRadius = .5f;
+
+    [Header("Vertical Collision")]
     [SerializeField] private Vector2 bottomColSize;
     [SerializeField] private Vector2 bottomColOffset;
 
-    [SerializeField] private float horizontalColRadius = .5f;
-    [SerializeField] private float bottomColRadius = .5f; 
+    [Space]
 
     public bool isOnRightWall = false;
     public bool isOnLeftWall = false;
+    public bool isOnWall = false;
+
+    [Space]
+
     public bool isGrounded = false;
 
     private void Update()
     {
         isOnRightWall = Physics2D.OverlapCircle((Vector2) transform.position + rightColOffset, horizontalColRadius, whatIsBlocks);
         isOnLeftWall = Physics2D.OverlapCircle((Vector2) transform.position + leftColOffset, horizontalColRadius, whatIsBlocks);
+        isOnWall = isOnLeftWall || isOnRightWall;
 
         isGrounded = Physics2D.OverlapBox((Vector2) transform.position + bottomColOffset, bottomColSize, 0f, whatIsBlocks);
     }
